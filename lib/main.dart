09a6d7b4cf57a360ffd14f4fa1b523f6a46cf1c3
MhmdAwad <mhmdawaddd@gmail.com>
2020-09-12 14:23:18 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:sarahah_chat/ui/screens/ChatScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
